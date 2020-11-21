c0d62a795a4ecb0eed1e8ed88c5a0af0e3f217b9
tobo <torgebo@fastmail.fm>
2020-11-21 08:38:12 +0100

    Rewrite day1 using more intuitive constructs.

--- day_1/index.js
+++ day_1/index.js
@@ -2,39 +2,31 @@ const fs = require('fs')
 
 const text = fs.readFileSync(process.stdin.fd, 'utf-8').trim()
 const textAr = text.split("")
+const opAr = textAr.map((a) => ((a == "(") ? 1 : -1))
 
-const operator = function(label) {
-	return (accumulator, currentValue) => {
-		if (currentValue == label) return accumulator + 1
-		return accumulator
+function walk() {
+	let state = 0
+	return function (vec) {
+		state += vec
+		return state
 	}
 }
 
-const up = operator('(')
-const down = operator(')')
+const walkedAtEnd = opAr.map(walk())
+const part1 = walkedAtEnd[walkedAtEnd.length - 1]
 
-const upDown = (ar => ar.reduce(up, 0) - ar.reduce(down, 0))
+console.log("Part 1: ", part1)
 
-console.log("Part 1: ", upDown(textAr))
-
-
-const barrierOp = function() {
-	return (accum, curVal, index, ar) => {
-		//const {floor, foundPos} = accum
-		const floor = accum[0]
-		const foundPos = accum[1]
-		if (floor == -1)  return accum;
-		if ((floor == 0) && (curVal == ')')) return [-1, index];
-		if (curVal == ')') return [floor - 1, -1];
-		if (curVal == '(') return [floor + 1, -1];
+function sweepLevelFromWalkedAtEnd(level) {
+	return function(acc, cur, index, ar) {
+		if (acc >= 0) return acc
+		if (cur == level){
+			return index
+		}
+		return -1
 	}
 }
 
-basementBreach = barrierOp()
 
-
-const ans =  textAr.reduce(basementBreach, [0, -1])
-const floor = ans[0]
-const index = ans[1]
-console.log("Part 2: ", floor)
-console.log("Part 2: ", index + 1, floor)
+const part2 = walkedAtEnd.reduce(sweepLevelFromWalkedAtEnd(-1), -1) + 1
+console.log("Part 2: ", part2) 
