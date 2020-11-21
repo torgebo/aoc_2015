b411b0464507ca1643d17ff03a40a142bdc1dc7f
tobo <torgebo@fastmail.fm>
2020-11-21 21:03:10 +0100

    Day 2, Part 2

--- day_2/index.js
+++ day_2/index.js
@@ -3,7 +3,8 @@ const fs = require('fs')
 const text = fs.readFileSync(process.stdin.fd, 'utf-8').trim()
 const lines = text.split("\n")
 const dims = lines.map(l => l.split("x").map(s => parseInt(s)))
-const faces = dims.map(dim => [dim[0] * dim[1], dim[0] * dim[2], dim[1] * dim[2]])
+const facePairs = dims.map(dim => [[dim[0], dim[1]], [dim[0], dim[2]], [dim[1], dim[2]]])
+const faces = facePairs.map(facePair => facePair.map(fp => fp[0] * fp[1]))
 
 const paperForEachPackage = faces.map(
 	face => (
@@ -15,4 +16,11 @@ const paperForEachPackage = faces.map(
 const part1 = paperForEachPackage.reduce((a, b) => a + b)
 console.log("Part 1: ", part1)
 
+const minFacePerim = facePairs.map(facepair => Math.min(...facepair.map(fp => 2 * (fp[0] + fp[1]))))
 
+
+const vol = dims.map(d => d[0] * d[1] * d[2])
+
+const part2 = minFacePerim.map((perim, ind) => perim + vol[ind]).reduce(
+	(a, b) => a + b)
+console.log("Part 2: ", part2)
